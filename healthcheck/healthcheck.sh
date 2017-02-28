57cb436d52f7273e563847f90015afe7a6faa7e4
Kim Pellikaan <kim@web-flight.nl>
2017-02-28 12:12:55 +0100

    Added a timer to the healthcheck to kill the container. Hoping to get more stability this way.

--- healthcheck/healthcheck.sh
+++ healthcheck/healthcheck.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+min_live_time=3600
+total_live_time=$(($min_live_time + ${CF_INSTANCE_INDEX} * 200))
+counter=0
+
+function killAllVcapProcesses {
+  killall -u vcap  
+}
+
 while :
 do
-  if ! pgrep -x "java" > /dev/null
-  then
-      killall -u vcap  
+  # if ! pgrep -x "java" > /dev/null
+  # then
+  #   killAllVcapProcesses 
+  #   break
+  # fi
+  if [ $total_live_time -lt $counter ]
+    then
+    killAllVcapProcesses
+    break
   fi
+  echo $counter
+  let counter=counter+1
   sleep 1
 done
